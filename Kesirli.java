e32ddd61d890bb24baace61af57199cf8fc39e65
KadirCengiz <115431038+KadirCengiz@users.noreply.github.com>
2022-12-01 12:06:35 +0300

    Add files via upload

---
+++ Kesirli.java
@@ -0,0 +1,20 @@
+public class Kesirli {
+    public static void main(String[] args) {
+        float t,x;
+        t=35+5;
+        x=t/4;
+        System.out.println(x);
+
+        float number1=3.14F;
+        float number2=3.14f;
+        double number3=3.14;
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+        float vFloat=3;
+        System.out.println(vFloat);
+        double vDouble=3.14;
+        System.out.println(vDouble);
+    }
+}
